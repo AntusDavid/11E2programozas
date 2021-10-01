06a979173f985dbdec905599289b62ea7deb1d3c
AntusDavid <91724194+AntusDavid@users.noreply.github.com>
2021-10-01 11:36:02 +0200

    Hazi feladat 45784

---
+++ 21.10.01/21.10.01/21.10.01/21.10.01/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21._10._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //feladat1();
+            //feladat2();
+            //feladat3();
+            //feladat4();
+            //feladat5();
+            //feladat6();
+            //feladat7();
+            //feladat8();
+            //feladat9();
+            //feladat10();
+            //feladat11();
+            //feladat12();
+            //feladat13();
+            //feladat14();
+            //feladat15();
+            //feladat16();
+            //feladat17();
+            //feladat18();
+            Console.WriteLine("enter");
+            Console.ReadLine();
+        }
+        static void feladat1()
+        {
+            Console.WriteLine("Hello world!");
+        }
+        static void feladat2()
+        {
+            Console.Write("Add meg a neved!");
+            string nev = Console.ReadLine();
+            Console.WriteLine("Szia {0}",nev);
+
+        }
+        static void feladat3()
+        {
+
+        }
+        static void feladat4()
+        {
+
+        }
+        static void feladat5()
+        {
+
+        }
+        static void feladat6()
+        {
+            Console.Write("Kérem a négyzet oldalát! a=");
+            double a = double.Parse(Console.ReadLine());
+            double k = 4 * a;
+            Console.WriteLine("A négyzet kerülete {0}",k);
+            double t = a * a;
+            Console.WriteLine("A négyzet területe {0}", t);
+            t = Math.Pow(a, 2);
+            Console.WriteLine("A négyzet területe {0}", t);
+        }
+        static void feladat7()
+        {
+
+        }
+        static void feladat8()
+        {
+
+        }
+        static void feladat9()
+        {
+            Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("c=");
+            int c = int.Parse(Console.ReadLine());
+            int legkisebb = Math.Min(a, Math.Min(b, c));
+            Console.WriteLine("A leg kisseb szám: {0}",legkisebb);
+        }
+        static void feladat10()
+        {
+
+        }
+        static void feladat11()
+        {
+
+        }
+        static void feladat12()
+        {
+
+        }
+        static void feladat13()
+        {
+
+        }
+        static void feladat14()
+        {
+
+        }
+        static void feladat15()
+        {
+
+        }
+        static void feladat16()
+        {
+
+        }
+        static void feladat17()
+        {
+
+        }
+        static void feladat18()
+        {
+
+        }
+    }
+}
